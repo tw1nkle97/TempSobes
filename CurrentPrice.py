9769838eb5b7145b3a7bb5d0fc1b353603741ee6
Ivan <grabovivan@gmail.com>
2023-03-14 21:38:23 +0300

    создали цикл для анализа цены

--- CurrentPrice.py
+++ CurrentPrice.py
@@ -1,5 +1,11 @@
 import ccxt
+import time
 
 exchange = ccxt.binance()
 symbol = 'ETH/USDT' # указываем символ торговли ETH/USDT
 
+pastPrice = None # предыдущая цена
+
+while True: # цикл, в котором запрашиваем актуальную цену каждую минуту, а затем проводим анализ
+    ticker = exchange.fetch_ticker(symbol)
+    price = ticker['last']
