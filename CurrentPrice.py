bf2402153e0ccdf8b56bf39b75051b970836e26c
Ivan <grabovivan@gmail.com>
2023-03-14 21:42:45 +0300

    добавил условие для анализа изменений цены

--- CurrentPrice.py
+++ CurrentPrice.py
@@ -9,3 +9,11 @@ pastPrice = None # предыдущая цена
 while True: # цикл, в котором запрашиваем актуальную цену каждую минуту, а затем проводим анализ
     ticker = exchange.fetch_ticker(symbol)
     price = ticker['last']
+
+    if pastPrice: 
+        changePrice = (price - pastPrice) / pastPrice * 100
+        if abs(changePrice) >= 1: # Если процент изменения цены >= 1%, выводим сообщение в консоль.
+            print(f'Price change: {changePrice}')
+
+    pastPrice = price
+    time.sleep(60)
